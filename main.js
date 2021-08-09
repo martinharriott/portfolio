7e3091408f8644cf380d97aa2df27b8600f1ff79
Martin Harriott <martin.roseharriott@gmail.com>
2021-08-09 12:05:05 -0400

    commit 3

--- main.js
+++ main.js
@@ -1,37 +1,12 @@
+console.log('js load');
+
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
-var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos <= currentScrollPos && $(window).width() < 680) {
-    document.getElementById("navbar").style.top = "-120px";
-  } else {
-    document.getElementById("navbar").style.top = "0";
-  }
-  prevScrollpos = currentScrollPos;
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos <= currentScrollPos && $(window).width() < 680) {
+        document.getElementById("navbar").style.top = "-120px";
+    } else {
+        document.getElementById("navbar").style.top = "0";
+    }
+    prevScrollpos = currentScrollPos;
 }
-
-$(document).ready(function () 
-{
-    $('.submit').click(function (event) 
-    {
-        console.log('Clicked');
-
-        var name = $('.name').val();
-        var email = $('.email').val();
-        var subject = $('.subject').val();
-        var message = $('.message').val();
-        var statusElm = $('.status');
-        statusElm.empty();
-
-        if (!(email.length > 5 && email.includes('@') && email.includes('.')))
-        {
-            event.preventDefault();
-            statusElm.append('<div>Email is invalid</div>');
-        }
-
-        if (message.length < 2)
-        {
-            event.preventDefault();
-            statusElm.append('<div>Message is invalid</div>')
-        }
-    })
-})
